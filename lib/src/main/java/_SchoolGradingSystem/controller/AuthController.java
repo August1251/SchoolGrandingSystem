4462e9c70d63cf8d426c9cb0e412b5e4234d7973
August1251 <mn939632@gmail.com>
2023-09-30 16:34:13 +0300

    update AuthController added authorization

--- lib/src/main/java/_SchoolGradingSystem/controller/AuthController.java
+++ lib/src/main/java/_SchoolGradingSystem/controller/AuthController.java
@@ -2,6 +2,9 @@ package _SchoolGradingSystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import _SchoolGradingSystem.dto.LoginDto;
 import _SchoolGradingSystem.entity.UserEntity;
 import _SchoolGradingSystem.service.AuthService;
 
@@ -43,6 +47,24 @@ public class AuthController {
 		authService.saveRegisteredUser(user);
 		
 		return "register";
+	}
+	
+	@GetMapping("/login")
+	public String loginPage(Model model) {
+		model.addAttribute("login", new LoginDto());
+		
+		return "login";
+	}
+	
+	@PostMapping("/login")
+	public String loginControl(@ModelAttribute("login") LoginDto login) {
+		Authentication authentication = new UsernamePasswordAuthenticationToken(
+				login.getUsername(),
+				login.getPassword());
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
+		return "login";
 	}
 	
 }
